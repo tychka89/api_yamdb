11fc7b9ca06d9f043dc7d5c9d775474121d4f9a6
Nataliya Rodikova <bachininang@gmail.com>
2022-11-09 21:26:35 +0500

    done1

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,7 +5,7 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.role == 'admin'
-            or request.user.is_superuser )
+            or request.user.is_superuser)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
