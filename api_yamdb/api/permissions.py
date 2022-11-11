6db1f3a715757cbcc5d3163898b8331526a236bc
Nataliya Rodikova <bachininang@gmail.com>
2022-11-11 20:08:56 +0500

    property

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,8 +4,7 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-            request.user.role == 'admin'
-            or request.user.is_superuser)
+            request.user.is_admin or request.user.is_superuser)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -21,6 +20,6 @@ class AuthorAdminModeratorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
-                or request.user.role == 'admin'
-                or request.user.role == 'moderator')
+                or request.user.is_admin
+                or request.user.is_moderator
+                or obj.author == request.user)
